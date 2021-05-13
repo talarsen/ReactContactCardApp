1762cb83c7b9ce0ed6c35e99b079b743b7476ea9
outdoorcoder <talarsen74@gmail.com>
2021-05-13 09:48:16 -0700

    A simple contact card app built with React. A simple contact card app built with React.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-function App() {
+function App(props) {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <div className="card">
         <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+          <h2 className=" name heading">{props.name}</h2>
+          <img className="circle-img"
+            src={props.img}
             alt="avatar_img"
           />
         </div>
         <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<div>
+<App 
+ name="Alex Hannold"
+ img="https://pbs.twimg.com/profile_images/521729433426599937/0nntVU9L_400x400.jpeg"
+ tel="+123 456 789"
+ email="Solo.com"/>
+
+<App
+ name="Conrad Anker"
+ img="https://d29zunrt9sid73.cloudfront.net/speaker_media/asset/36801/large_70_Screen_Shot_2019-04-12_at_7.05.57_AM.png"
+ tel="+123 456 789"
+ email="mountains.com"/>
+
+
+<App
+ name="Lynn Hill"
+ img="https://i1.sndcdn.com/artworks-PmGItQUlHGP6Cn49-a2Vtqg-t500x500.jpg"
+ tel="+123 456 789"
+ email="climb.com"/>
+</div>,
+
+document.getElementById("root"));
 
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
